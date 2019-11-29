bb686f18b1698aca8ed14a20dd39cbbd06df5610
Katie Rose <katiefelten@gmail.com>
2019-11-29 18:49:33 -0500

    :zap: added project preview

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,51 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export default () => <p>Add code</p>;
+import Layout from '../components/layout';
+import ProjectPreview from '../components/project-preview';
+
+const Home = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allProjectsJson {
+        edges {
+          node {
+            title
+            slug
+            description
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const projects = data.allProjectsJson.edges;
+
+  return (
+    <Layout>
+      {projects.map(({ node: project }) => {
+        const title = project.title;
+        const description = project.description;
+        const slug = project.slug;
+        const imageData = project.image.childImageSharp.fluid;
+
+        return (
+          <ProjectPreview
+            title={title}
+            description={description}
+            slug={slug}
+            imageData={imageData}
+          />
+        );
+      })}
+    </Layout>
+  );
+};
+
+export default Home;
