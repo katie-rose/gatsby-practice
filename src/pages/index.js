9231ecdb647744cd63dda9ed7749e153f6908843
Katie Rose <katiefelten@gmail.com>
2020-01-03 13:08:22 -0500

    :zap: a basic layout appears

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Katie Rose Felten</p>
     <Link to="/about/">Learn about me &rarr;</Link>
-  </>
+  </Layout>
 );
