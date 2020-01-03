ad5b2b984a0c70ec840ff76c33b3d43a086b7dea
Katie Rose <katiefelten@gmail.com>
2020-01-03 14:30:00 -0500

    :zap: added images to posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
+import Hero from '../components/hero';
 import PostPreview from '../components/post-preview';
-import ReadLink from '../components/read-link';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Some words about stuff.</p>
-      <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
-
-      <h2>Some of my work</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>See my work</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
